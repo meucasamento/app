cd6916736e434c14ec276565c500f8de704ba556
Adriano Souza Costa <adriano.souza@consultaremedios.com.br>
2020-02-03 23:27:57 -0300

    Melhorando forma de calculo de dias restantes para o casamento

--- src/redux/reducers/weeding.reducer.ts
+++ src/redux/reducers/weeding.reducer.ts
@@ -1,20 +1,20 @@
 import pt from 'date-fns/locale/pt';
-import { format } from 'date-fns'
+import { format, differenceInDays, formatDistanceStrict } from 'date-fns'
 import { WeddingState } from './../types/wedding.types'
 
 export function weddingReducer(): WeddingState {
     const now = new Date()
     const date = new Date(2020, 5, 6, 19, 0, 0)
-    const diffTime = Math.abs(date.getTime() - now.getTime())
-    const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24))
     const formattedDate = format(date, "'Dia' dd 'de' MMMM',\n' iiii 'às 'HH:mm'h'", { locale: pt })
+    const daysLeft = differenceInDays(date, now)
+    const formattedDaysLeft = formatDistanceStrict(date, now, { unit: "day", locale: pt})
 
     return {
         wedding: {
             date,
             formattedDate,
-            daysLeft: diffDays,
-            formattedDaysLeft: diffDays + " dias"
+            daysLeft,
+            formattedDaysLeft
         }
     }
 }
