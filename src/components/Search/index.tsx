59ff30b696ce652853c698f5b1168a44ca84756c
Adriano Souza Costa <adriano.souza@consultaremedios.com.br>
2020-02-04 22:12:43 -0300

    Criando handler que emite as alterações feitas no campo de busca

--- src/components/Search/index.tsx
+++ src/components/Search/index.tsx
@@ -11,18 +11,23 @@ import styles from './stylesheet'
 
 type Props = {
     value?: string
-    placeholder?: string
+    placeholder?: string,
+    isEnabled: boolean,
+    onChangedText?: (text: string) => void
 }
 
 export default class Search extends Component<Props> {
     static defaultProps = {
+        isEnabled: true,
         placeholder: "Pesquise por aqui"      
     }
 
     render() {
         const {
             value,
-            placeholder
+            placeholder,
+            onChangedText,
+            isEnabled
         } = this.props
 
         return(
@@ -33,7 +38,9 @@ export default class Search extends Component<Props> {
                         style={styles.textInput}
                         placeholder={placeholder}
                         clearButtonMode="while-editing"
-                        returnKeyType="done"/>
+                        returnKeyType="done"
+                        onChangeText={onChangedText}
+                        editable={isEnabled}/>
                 </SafeAreaView>
             </View>
         )
