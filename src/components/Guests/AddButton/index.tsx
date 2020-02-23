a6eea4984542205995874412d56ae6b543f115a9
Adriano Souza Costa <adriano.souza@consultaremedios.com.br>
2020-02-23 17:37:34 -0300

    Animação no botão para adicionar um novo convidado

--- src/components/Guests/AddButton/index.tsx
+++ src/components/Guests/AddButton/index.tsx
@@ -1,8 +1,7 @@
 import React from 'react'
 import {
-    TouchableWithoutFeedback,
-    TouchableOpacity,
-    View
+    Animated,
+    TouchableOpacity
 } from 'react-native'
 
 import styles from './styles'
@@ -12,14 +11,38 @@ type Props = {
     children?: any
 }
 
-const AddButton = (props: Props) => (
-    <View style={styles.container}>
-        <TouchableOpacity 
-            style={styles.touchable}
-            onPress={props.onPressed}>
-            {props.children}
-        </TouchableOpacity>
-    </View>
-)
+const AddButton = (props: Props) => {
+    const animatedValue = new Animated.Value(1)
+
+    const handlePressIn = () => {
+        Animated.spring(animatedValue, {
+            toValue: .8       
+        }).start()
+    }
+
+    const handlePressOut = () => {
+        Animated.spring(animatedValue, {
+            toValue: 1,
+            friction: 3,
+            tension: 40
+        }).start()
+    }
+
+    const animatedStyle = {
+        transform: [{ scale: animatedValue }]
+    }
+
+    return (
+        <Animated.View style={[styles.container, animatedStyle]}>
+            <TouchableOpacity 
+                style={styles.touchable}
+                onPress={props.onPressed}
+                onPressIn={handlePressIn}
+                onPressOut={handlePressOut}>
+                {props.children}
+            </TouchableOpacity>
+        </Animated.View>
+    )
+}
 
 export default AddButton
