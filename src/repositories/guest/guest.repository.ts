15ddb6675a5dea5be243ea33639d5809e92c775e
Adriano Souza Costa <adriano.souza@consultaremedios.com.br>
2020-02-25 08:23:51 -0300

    Completando os serviços do repositório do convidados

--- src/repositories/guest/guest.repository.ts
+++ src/repositories/guest/guest.repository.ts
@@ -1,10 +1,13 @@
 import Guest from "../../models/guest.model";
 import api from '../../services/api'
-import Pagination from "../../models/response/pagination.response";
+import { PaginationResult } from "../../models/response/pagination.response";
 import sessionMananger, { Session } from "../../utils/sessionMananger";
 
 export interface GuestRepositoryInterface {
-    guests(page: number, limit: number): Promise<Pagination<Guest>>
+    guests(page: number, limit: number): Promise<PaginationResult<Guest>>
+    store(guest: Guest): Promise<Guest>
+    update(guest: Guest): Promise<Guest>
+    delete(guest: Guest): Promise<void>
 }
 
 class GuestRepository implements GuestRepositoryInterface {
@@ -15,7 +18,7 @@ class GuestRepository implements GuestRepositoryInterface {
         this.session = session
     }
 
-    guests = async (page: number = 1, limit: number): Promise<Pagination<Guest>> => {
+    guests = async (page: number = 1, limit: number = 10): Promise<PaginationResult<Guest>> => {
         const params = {
             page,
             limit
@@ -25,7 +28,31 @@ class GuestRepository implements GuestRepositoryInterface {
             authorization: await this.session.getToken()
         }
 
-        return await api.request<Pagination<Guest>>("guests", "get", { params, headers })
+        return await api.request<PaginationResult<Guest>>("guests", "get", { params, headers })
+    }
+
+    store = async (guest: Guest): Promise<Guest> => {
+        const headers = {
+            authorization: await this.session.getToken()
+        }
+
+        return await api.request<Guest>("guest", "post", { params: guest, headers })
+    }
+    
+    update = async (guest: Guest): Promise<Guest> => {
+        const headers = {
+            authorization: await this.session.getToken()
+        }
+
+        return await api.request<Guest>("guest", "patch", { params: guest, headers })
+    }
+
+    delete = async (guest: Guest): Promise<void> => {
+        const headers = {
+            authorization: await this.session.getToken()
+        }
+
+        return await api.request("guest", "delete", { params: { id: guest._id }, headers })
     }
 
 }
