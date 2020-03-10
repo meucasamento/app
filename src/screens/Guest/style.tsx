2ae713988d0652f798c0a5552a26a4d855c17fe7
Adriano Souza Costa <adriano.souza@consultaremedios.com.br>
2020-03-09 21:40:54 -0300

    Melhorando exibição das células dos convidados

--- src/screens/Guest/style.tsx
+++ src/screens/Guest/style.tsx
@@ -9,11 +9,6 @@ export default StyleSheet.create({
         color: "white"
     },
     list: {
-        // backgroundColor: 'white'
-    },
-    separator: {
-        // height: 1,
-        // marginLeft: 16,
-        // backgroundColor: '#F0F0F0'
+        backgroundColor: 'white'
     }
 })
